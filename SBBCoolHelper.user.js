0824014c0351afcb27da5d2f1554c104011a0bf4
ducng99 <tom@ducng.dev>
2022-02-01 16:01:07 +1300

    v1.1.4 Improved UI/UX

--- SBBCoolHelper.user.js
+++ SBBCoolHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SBB Cool Helper
 // @namespace    maxhyt.SBBCoolHelper
-// @version      1.1.3.1
+// @version      1.1.4.0
 // @description  Add VIP features to SBB site
 // @license      AGPL-3.0-or-later
 // @copyright    2022. Thomas Nguyen
